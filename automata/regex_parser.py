a882f8a2567896845eb8a2914ae0ae6b17e8ced1
YAZEED MOWAFAQ ALI KANAAN <yazeedkanan513@gmail.com>
2026-05-15 10:22:45 +0300

    Implement RegexParser with concatenation and postfix

--- automata/regex_parser.py
+++ automata/regex_parser.py
@@ -1,1 +1,39 @@
+class RegexParser:
+    precedence = {
+        "*": 3,
+        ".": 2,
+        "|": 1
+    }
 
+    def add_concat(self, regex):
+        result = ""
+        for i in range(len(regex)):
+            result += regex[i]
+            if i + 1 < len(regex):
+                if regex[i] not in "(|" and regex[i+1] not in "*)|":
+                    result += "."
+        return result
+
+    def to_postfix(self, regex):
+        regex = self.add_concat(regex)
+        output = ""
+        stack = []
+
+        for c in regex:
+            if c.isalnum():
+                output += c
+            elif c == "(":
+                stack.append(c)
+            elif c == ")":
+                while stack and stack[-1] != "(":
+                    output += stack.pop()
+                stack.pop()
+            else:
+                while stack and stack[-1] != "(" and self.precedence[c] <= self.precedence[stack[-1]]:
+                    output += stack.pop()
+                stack.append(c)
+
+        while stack:
+            output += stack.pop()
+
+        return output
